29ba4671774f2d4e61974ba885c51b2607936117
stefanosambruna <ste23s@hotmail.it>
2017-10-11 02:42:05 +0200

    3am commit ... need to change the inverst operation. Find complementar function

--- kubmic.py
+++ kubmic.py
@@ -32,10 +32,14 @@ class Kubmic(Problem):
                         elif direction == "H": new_state.grid[a][b - space] = state.grid[a][b]
                     if direction == "V": action = "Col " + str(a + 1) + ": "
                     else: action = "Row " + str(a + 1) + ": "
+                    #TODO: check this
                     if state.nbr - space < 0: action += "-"
                     else: action += "+"
                     action += str(space)
                     yield action, new_state
+
+    def initial_test(self, state):
+        return self.initial == state
 
 
 ###############
@@ -82,25 +86,106 @@ def readInstanceFile(filename):
 
     return grid_init, grid_goal
 
-def add_into_the_fringe(fringe, element, closed):
-    fringe.append(element)
-    closed[element] = True
+def add_into_the_queue(queue, new_node, closed):
+    queue.append(new_node)
+    closed[new_node.state] = True
 
-def bidirectiona_breadth_search(problem, qi, qg):
+    '''
+    
+    
+            for w in range(0, len(path_to_goal) - 2):
+            path_to_bridge[w + 1].parent = path_to_bridge[w]
+            path_to_bridge[w].state = deepcopy(path_to_bridge[w + 1])
+        path_to_bridge[0].parent = path_to_goal[len(path_to_goal) - 1]
+        return path_to_goal[0]'''
+
+def success(problem,goal_node, qi, qg, initial):
+    if (problem.initial == goal_node.state):
+        return goal_node.path.reverse()
+    elif (problem.goal == goal_node.state):
+        return goal_node.path
+    if initial:
+        bridge_node = list(filter(lambda e: e.state == goal_node.state, qg.A))[0]
+        path_to_goal = bridge_node.path()
+        path_to_bridge = goal_node.path()
+        path_to_bridge.reverse()
+
+        path_to_goal[1].parent=path_to_bridge[-1]
+
+        for n in range(1,len(path_to_goal)-1):
+            path_to_goal[n+1].parent=path_to_goal[n]
+
+        for n in range(1,len(path_to_goal) - 1):
+           path_to_goal[len(path_to_goal) - n].action=path_to_goal[len(path_to_goal) - n -1].action
+
+        print("PATH TO BRIDGE")
+        for n in path_to_bridge:
+            print(
+                n.action)  # assuming the comment attribute of state contains a relevant string (e.g. describing the current move)
+            print(n.state)  # assuming that the __str__ function of state outputs the correct format
+            print()
+        print("PATH TO GOAL")
+        for n in path_to_goal:
+            print(n.action)
+            print(n.state)  # assuming that the __str__ function of state outputs the correct format
+            print()
+
+
+    else:
+        bridge_node = list(filter(lambda e: e.state == goal_node.state, qi.A))[0]
+        path_to_goal = goal_node.path()
+        path_to_bridge = bridge_node.path().reverse()
+        print("PATH TO BRIDGE")
+        for n in path_to_bridge:
+            print(
+                n.action)  # assuming the comment attribute of state contains a relevant string (e.g. describing the current move)
+            print(n.state)  # assuming that the __str__ function of state outputs the correct format
+            print()
+        print("PATH TO GOAL")
+        for n in path_to_goal:
+            print(n.action)
+            print(n.state)  # assuming that the __str__ function of state outputs the correct format
+            print()
+
+
+def bidirectional_breadth_search(problem, qi, qg):
+    inverse_problem = Kubmic(problem.goal, problem.initial)
     closed = {}
-    qi.append(problem.initial)
-    closed[problem.initial] = True
-    qg.append(problem.goal)
-    closed[problem.goal] = True
+    add_into_the_queue(qi, Node(problem.initial), closed)
+    add_into_the_queue(qg, Node(problem.goal), closed)
+    while qi.__len__() > 0 and qg.__len__() > 0:
+        if qi.__len__() > 0:
+            node_x = qi.pop()
+            if problem.goal_test(node_x.state):
+                return node_x
+            elif len(list(filter(lambda e: e.state == node_x.state, qg.A))) != 0:
+                return success(problem, node_x, qi, qg, True)  #problem,goal_node, qi, qg, initial
+            for x in node_x.expand(problem):
+                if x.state not in closed:
+                    add_into_the_queue(qi, x, closed)
+        if qg.__len__() > 0:
+            node_x = qg.pop()
+            if problem.initial_test(node_x.state):
+                return node_x
+            elif len(list(filter(lambda e: e.state == node_x.state, qi.A))) != 0:
+                return success(problem, node_x, qi, qg, False)
+            for x in node_x.expand(inverse_problem):
+                if x not in closed:
+                    add_into_the_queue(qg, x, closed)
+    return print("FAILURE")
 
+
+
+
+            #fringe.extend(node.expand(problem))
 
 
 #####################
 # Launch the search #
 #####################
 
-grid_init, grid_goal = readInstanceFile(sys.argv[1])
-#grid_init, grid_goal = readInstanceFile("/Users/stefanosambruna/PycharmProjects/AIassignment1/instances/a01")
+#grid_init, grid_goal = readInstanceFile(sys.argv[1])
+grid_init, grid_goal = readInstanceFile("/Users/stefanosambruna/PycharmProjects/AIassignment1/instances/a01")
 init_state = State(grid_init)
 goal_state = State(grid_goal)
 # print('- Instance: --')
@@ -112,19 +197,20 @@ goal_state = State(grid_goal)
 problem = Kubmic(init_state, goal_state)
 
 # example of bfs graph search
-node = iterative_deepening_search(problem)
-
+#node = iterative_deepening_search(problem)
+bidirectional_breadth_search(problem, FIFOQueue(), FIFOQueue())
 # example of print
-path = node.path()
+'''path = node.path()
 path.reverse()
 
 
 def comments(comm):
     return '\033[1;36;40m' + comm + '\033[0m'
-
+'''
 then = datetime.datetime.now()
-#print("- Duration: " + str(then-now))
+print("- Duration: " + str(then-now))
 #print('Number of moves: ' + str(node.depth))
+'''
 print(comments(
         '\u2193 init'))  # assuming the comment attribute of state contains a relevant string (e.g. describing the current move)
 print(path[0].state)  # assuming that the __str__ function of state outputs the correct format
@@ -135,4 +221,4 @@ for n in path[1:]:
     print(n.state)  # assuming that the __str__ function of state outputs the correct format
     print()
 
-
+'''
